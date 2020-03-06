eadfd7a7de5144eb0a73bee8c6c24cdd667e8060
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-03-06 18:45:06 -0500

    switched the layout. minor change

--- react-game/src/components/Phrase.js
+++ react-game/src/components/Phrase.js
@@ -11,13 +11,6 @@ const Phrase = () => {
     )
 }
 
-const RandomPhrase = () => {
-    var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
-    var random = Math.floor(Math.random() * phrases.length);
-    var getRandom = phrases[random].name;
-    return (getRandom)
-}
-
 const DisplayPhrase = props => {
     console.log(props)
     const spread = [...props.phrase] // place phrase letters into individual arrays
@@ -26,6 +19,15 @@ const DisplayPhrase = props => {
     );
     return (
         <ul>{listPhrase}</ul>
+    )
+}
+
+const RandomPhrase = () => {
+    var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
+    var random = Math.floor(Math.random() * phrases.length);
+    var getRandom = phrases[random].name;
+    return (
+        getRandom
     )
 }
 
