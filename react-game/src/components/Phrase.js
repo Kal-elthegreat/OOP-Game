349052704b0458d21404a2996ee7a96dfcf9be44
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-03-06 18:34:38 -0500

    built out the random phrase functionality and displaying it to the FE

--- react-game/src/components/Phrase.js
+++ react-game/src/components/Phrase.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const Phrase = () => {
+    const phrase = RandomPhrase();
     return(
         <div id="phrase" className="section">
-            <ul></ul>
+            <ul><DisplayPhrase phrase = {phrase}/></ul>
         </div>
     )
 }
+
+const RandomPhrase = () => {
+    var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
+    var random = Math.floor(Math.random() * phrases.length);
+    var getRandom = phrases[random].name;
+    return (getRandom)
+}
+
+const DisplayPhrase = props => {
+    console.log(props)
+    const spread = [...props.phrase] // place phrase letters into individual arrays
+    const listPhrase = spread.map((letter,index) =>  // creates li & adds letters to each one
+        letter === ' ' ? <li key={index} className='hide space'>{letter}</li> : <li key={index} className='hide letter'>{letter}</li>
+    );
+    return (
+        <ul>{listPhrase}</ul>
+    )
+}
+
+DisplayPhrase.propTypes = {
+    phrase: PropTypes.string
+};
 
 export default Phrase;
