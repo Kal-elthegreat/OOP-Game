7eb661ac8449f4a97934ed76a9ebc44933c914ab
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-03-06 19:04:32 -0500

    made some changes for code clarity

--- react-game/src/components/Phrase.js
+++ react-game/src/components/Phrase.js
@@ -3,31 +3,34 @@ import PropTypes from 'prop-types';
 
 
 const Phrase = () => {
-    const phrase = RandomPhrase();
+
+    const randomPhrase = () => {
+        var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
+        var random = Math.floor(Math.random() * phrases.length);
+        var getRandom = phrases[random].name;
+        return (
+            getRandom
+        )
+    }
+    var selectedPhrase = randomPhrase();
+
     return(
         <div id="phrase" className="section">
-            <ul><DisplayPhrase phrase = {phrase}/></ul>
+            <ul><DisplayPhrase phrase = {selectedPhrase}/></ul>
         </div>
     )
 }
 
 const DisplayPhrase = props => {
     console.log(props)
-    const spread = [...props.phrase] // place phrase letters into individual arrays
-    const listPhrase = spread.map((letter,index) =>  // creates li & adds letters to each one
-        letter === ' ' ? <li key={index} className='hide space'>{letter}</li> : <li key={index} className='hide letter'>{letter}</li>
-    );
+    const listPhrase = 
+    [...props.phrase]
+        .map((letter,index) =>
+            letter === ' ' ? <li key={index} className='hide space'>{letter}</li> : <li key={index} className='hide letter'>{letter}</li>
+        );
+
     return (
         <ul>{listPhrase}</ul>
-    )
-}
-
-const RandomPhrase = () => {
-    var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
-    var random = Math.floor(Math.random() * phrases.length);
-    var getRandom = phrases[random].name;
-    return (
-        getRandom
     )
 }
 
