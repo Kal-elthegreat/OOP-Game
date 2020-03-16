3fa210e2484264cb82e03d49113fe0b0054a5223
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-03-16 13:25:27 -0400

    change the layout of the files and got a working chosenLetter func

--- react-game/src/components/GameDisplay.js
+++ react-game/src/components/GameDisplay.js
@@ -6,12 +6,45 @@ import Keyboard from './Keyboard';
 import Scoreboard from './Scoreboard';
 
 const GameDisplay = () => {
+    let keyVal;
+
+    const randomPhrase = () => {
+        var phrases = [{ name: 'prestige worldwide' }, { name: 'jack jack has powers' }, { name: 'javascript is life' }, { name: 'to infinity and beyond' }, { name: 'just keep swimming' }];
+        var random = Math.floor(Math.random() * phrases.length);
+        var getRandom = phrases[random].name;
+        return (
+            getRandom
+        )
+    }
+    var selectedPhrase = randomPhrase();
+
+    const handleKey = (e) => {
+        if(e.target.className === 'key'){
+        keyVal = e.target.innerHTML;
+        }
+        chosenLetter(keyVal)
+        
+    }
+    const chosenLetter = (val) => {
+        if(val !== undefined) {
+            [...selectedPhrase].map(letter => {
+                if (letter === val) {
+                // change the class to show 
+                // make key unclickable - good
+                } else {
+                    //remove life
+                    //make key unclickable- bad
+                }
+            })
+        }
+    }
+
     return (
         <Fragment>
             <Overlay />
             <Banner />
-            <Phrase />
-            <Keyboard />
+            <Phrase randomPhrase={selectedPhrase}/>
+            <Keyboard handleKey={handleKey} onClick={chosenLetter}/>
             <Scoreboard />
         </Fragment> 
     )
