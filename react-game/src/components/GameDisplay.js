7093905499a06235008e79c10f4ff6cb5d5b2080
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-03-22 23:07:33 -0400

    func works when answer incorrect, now need to figure how to remove lives

--- react-game/src/components/GameDisplay.js
+++ react-game/src/components/GameDisplay.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { useReducer,Fragment } from 'react';
 import Overlay from './Overlay';
 import Banner from './Banner';
 import Phrase from './Phrase';
 import Keyboard from './Keyboard';
 import Scoreboard from './Scoreboard';
 
+// const reducer = (state,action) => {
+
+// }
+
 const GameDisplay = () => {
     
+    // const [key,dispatch] = useReducer(reducer, null);
+
     const handleKey = (e) => {
         if(e.target.className === 'key'){
             let keyVal = e.target;
@@ -16,14 +22,20 @@ const GameDisplay = () => {
 
     const checkLetter = (button) => {
         var listedPhrase = document.getElementsByClassName('letter');
+        let isMatch = false;
         if(button !== undefined) {
             [...listedPhrase].map(letter => {
-                if (letter.innerHTML === button.innerHTML) {
+                if(letter.innerHTML === button.innerHTML){
                     letter.className = 'show';
-                    button.className = 'chosen';
-                    button.disabled = true;
+                    isMatch = true;
                 }
             })
+            if(isMatch) {
+                button.className = 'chosen';
+            } else {
+                button.className = 'wrong'
+            }
+            button.disabled = true;
         }
     }
 
